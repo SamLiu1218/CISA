fb647d7fcf9cdf8a9e03b7beb011783fbab73351
SamLiu1218 <78160723+SamLiu1218@users.noreply.github.com>
2023-05-16 00:17:48 -0400

    Upload all

---
+++ CISA.py
@@ -0,0 +1,306 @@
+import numpy as np
+import pandas as pd
+import scipy
+from skimage import morphology
+
+def CISA(img_dict, cell_df, cellmask, boundmask, neighbor_classes, expressing_markers, cell_list = None, target_classes = ['T'], n_bootstraps = 1000, e_width = 2, bound_width = 0, dropna=True, class_col = 'Class', cell_id_col = 'Cell', keep_cols = ['Intratumoral'], excluded = ['T']):
+    """
+    Calculates CISA scores between given center cell types and their neighboring cell types for given markers. The default inputs are for calculating synapses between T cells and neighboring cells. Make sure to check all input arguments if applied to other cell types.
+
+    Output: a dataframe where each row is a contacting center-neighbor cell pair.
+
+    Input (required):
+
+        img_dict: {marker: 2d image}. Input single channel images as a dictionary.
+
+        cell_df: dataframe of cell information where each row is a cell.
+
+        cellmask: 2D int ndarray. Image mask of cells. Pixel values should be integers that matches cell ID in cell_df.
+
+        boundmask: 2D int ndarray. Image mask of cell boundaries. Pixel values should be integers that matches cell ID in cell_df.
+
+        neighbor_classes: list of neighboring classes. Other classes are not included in the result dataframe, but overlaping 
+
+        expressing_markers: list of markers that are expressed in the center cell types. Their background level will be calculated as the average level in other cell types. All other markers that are not listed but included in img_dict will be treated as expressed in the center cell types and their background level will be calculated as the average level in center cell types.
+
+    Input (optional):
+        cell list: list of cell IDs of center cells to compute CISA. If None, all cells are considered.
+
+        target_classes: list of center cell types. Default: ['T'].
+
+        n_bootstrap: number of bootstraps for the null model. Default: 1000
+
+        e_width: width of dilation to calculate overlapping. Default: 2 pixels.
+
+        bound_width: width of cell boundary dilation width before calculating overlapping. 0 = use raw bound. Default: 0 pixel.
+
+        dropna: Bool, if drops invalid synapses. Default: True
+
+        class_col: the colname for the cell type column in cell_df. Default: 'Class'
+
+        cell_id_col: the colname for the cell ID column in cell_df. Default: 'Cell'
+
+        keep_cols: list of colnames that should be kept in the output dataframe. Default: []
+        
+        excluded: list of cell between which and the center cells the contacted regions should be completely ignored in calculation. One example is to reduce the influence of signals from neighboring same type of cells. Default: ['T'].
+
+    """
+
+    markers = list(img_dict.keys())
+    # < bgl >
+    valarr = cell_df[class_col].isin(target_classes).values
+    valarr_T = np.append([0], valarr)
+    mask_T = valarr_T[cellmask]
+    valarr_nonT = np.append([0], np.logical_not(valarr))
+    mask_nonT = valarr_nonT[cellmask]
+
+    bgl_dict = {}
+    for m in markers:
+        if m in expressing_markers:
+            bgl_dict[m] = np.sum(img_dict[m]*mask_nonT)/np.sum(mask_nonT)
+        else:
+            bgl_dict[m] = np.sum(img_dict[m]*mask_T)/np.sum(mask_T)
+
+    # </ bgl >
+
+    # < init >
+    diam = morphology.diamond(e_width)
+    headers = ['Cell','Class','Neighbor','Neighbor_Class','area_c','area_nc'] + list(keep_cols)
+    headers = headers + [f"{m}_{h}" for h in ["c", "nc", "CISA", "miu","sigma", "z", "p"] for m in list(img_dict.keys()) ]
+    syn_df = pd.DataFrame(columns=headers)
+    # </ init >
+
+    # < cell list >
+    if cell_list is None:
+        cell_list = cell_df.loc[cell_df[class_col].isin(target_classes), cell_id_col].values
+
+    # </ cell list >
+
+    # main
+    for cell_id in cell_list:
+        # skip non existing cell id
+        if np.sum(boundmask == cell_id) == 0:
+            continue
+
+        # < neighbor list >
+        bm, [ytop,ybot,xleft,xright] = maskcrop(boundmask, boundmask == cell_id, padding=10, return_range=True)
+        
+        bm_cur = bm == cell_id
+        
+        for w in range(bound_width):
+            bm_cur = morphology.binary_dilation(bm_cur)
+            
+        neighbor_ls = np.unique(bm).astype(int)
+        neighbor_ls = [i for i in neighbor_ls if i not in [0, cell_id]]   
+        # </ neighobor list >
+
+        # < exclusion >
+        for neighbor_id in neighbor_ls:
+            interactee_c = cell_df.loc[cell_df[cell_id_col] == neighbor_id, class_col].values[0]
+            if interactee_c not in excluded:
+                continue
+            bm_nb = bm == neighbor_id 
+            bm_nb_dil = morphology.binary_dilation(bm_nb, diam)
+            mask_c = np.logical_and(bm_cur, bm_nb_dil)
+            bm_cur = bm_cur^mask_c
+        # </ exclusion >
+
+        # < contacting & non contacting masks >
+        dict_nc = {c:bm_cur for c in neighbor_classes}
+        dict_c = {}
+        
+        # find mask_nc for each class
+        for neighbor_id in neighbor_ls:
+            interactee_c = cell_df.loc[cell_df[cell_id_col] == neighbor_id, class_col].values[0]
+            if interactee_c not in neighbor_classes:
+                continue
+
+            bm_nb = bm == neighbor_id 
+
+            bm_nb_dil = morphology.binary_dilation(bm_nb, diam)
+            
+            mask_c = np.logical_and(bm_cur, bm_nb_dil)
+            
+            # if not contacted at all
+            if np.sum(mask_c) == 0:
+                continue
+                
+            # if contacted by a tiny area, dilate by one additional pixel
+            if np.sum(mask_c) < 3:
+                bm_nb_dil = morphology.binary_dilation(bm_nb_dil, morphology.diamond(1))
+                mask_c = np.logical_and(bm_cur, bm_nb_dil)
+                
+            mask_nc = np.logical_and(dict_nc[interactee_c], np.logical_not(bm_nb_dil))
+            dict_nc[interactee_c] = mask_nc
+            dict_c[neighbor_id] = mask_c
+        # </ contacting & non contacting masks >
+        
+        # CISA and null model
+        for neighbor_id in dict_c.keys():
+            interactee_c = cell_df.loc[cell_df[cell_id_col] == neighbor_id, class_col].values[0]
+            if interactee_c not in neighbor_classes:
+                continue
+            mask_nc = dict_nc[interactee_c] 
+            mask_c = dict_c[neighbor_id] 
+            
+            area_c = np.sum(mask_c)
+            area_nc = np.sum(mask_nc)
+                
+            if area_nc == 0:
+                continue
+            if area_c == 0:
+                continue
+                
+            dict_new = {'Cell':cell_id, 
+                        'Class':cell_df.loc[cell_df[cell_id_col]==cell_id, class_col].values[0], 
+                        'Neighbor':neighbor_id, 
+                        'Neighbor_Class':interactee_c}
+            for c in keep_cols:
+                if c in cell_df.columns:
+                    dict_new[c] = cell_df.loc[cell_df[cell_id_col] == cell_id, c].values[0]
+
+            dict_new['area_c'] = area_c
+            dict_new['area_nc'] = area_nc
+            
+            null_df = calculate_CISA_null_multi(
+                {m:img_dict[m][ytop:ybot, xleft:xright] for m in img_dict.keys()}, 
+                bm, area_c, bgl_dict, n_bootstraps=n_bootstraps)
+            for m in img_dict.keys():
+                img = img_dict[m]
+                bgl = bgl_dict[m]
+
+                img_crop = img[ytop:ybot, xleft:xright]
+
+                dict_new[f'{m}_c'] = np.sum(img_crop[mask_c])/area_c if area_c > 0 else bgl
+                dict_new[f'{m}_nc'] = np.sum(img_crop[mask_nc])/area_nc if area_nc > 0 else bgl
+
+                dict_new[f'{m}_CISA'] = calculate_CISA(img_crop, mask_c, mask_nc, bgl)
+
+
+                dict_new[f'{m}_miu'] = null_df.at[m, 'miu']
+                
+                dict_new[f'{m}_sigma'] = null_df.at[m, 'sigma']
+                
+                dict_new[f'{m}_z'] = (dict_new[f'{m}_CISA'] - dict_new[f'{m}_miu'])/dict_new[f'{m}_sigma']
+                
+                dict_new[f'{m}_p'] = scipy.stats.norm.sf(abs(dict_new[f'{m}_z']))
+
+            syn_df = syn_df.append(dict_new, ignore_index=1)
+    if dropna:
+        return syn_df.dropna()
+    else:
+        return syn_df
+    
+
+def M2E(M, nonzero=True):
+    '''
+    Convert an adjacency matrix to an edge list.
+    '''
+    df = pd.DataFrame(M)
+    df = df.stack().reset_index()
+    if nonzero:
+        return df.loc[df.iloc[:,2]>0, :].values[:,:2]
+    else:
+        return df.values[:,:2]
+
+def maskcrop(im, mask=None, padding=None, return_range=False):
+    mask = mask.copy()
+    im_size = im.shape
+    if type(mask)==type(None):
+        mask = (im>0)
+    mask = mask*1
+    
+    xleft = np.argwhere(np.any(mask, axis=0))[0][0]
+    xright = np.argwhere(np.any(mask, axis=0))[-1][0]
+
+    ytop = np.argwhere(np.any(mask, axis=1))[0][0]
+    ybot = np.argwhere(np.any(mask, axis=1))[-1][0]
+
+    if padding == None:
+        padding = int(0.05*max(xright-xleft, ybot-ytop))
+    xleft = int(max(xleft - padding, 0))
+    xright = int(min(xright + padding, im_size[1]))
+    ytop = int(max(ytop - padding, 0))
+    ybot = int(min(ybot + padding, im_size[0]))
+    if len(im.shape) == 3:
+        im_new = im[ytop:ybot,xleft:xright,:]
+    else:
+        im_new = im[ytop:ybot,xleft:xright]
+    if return_range:
+        return im_new, [ytop,ybot,xleft,xright]
+    else:
+        return im_new
+    
+    
+def calculate_CISA_null_multi(img_dict, bm, area_c, bgl_dict, n_bootstraps=1000):
+    """
+    returns a df in the form of [m, ['miu','sigma']]
+    """
+    df = pd.DataFrame(columns = ['sigma','miu'], index = list(img_dict.keys()))
+    coord_list = M2E(bm)
+    grow_seed = area_c
+    
+    box = [ ( x, y ) for x in [-1, 0, 1] for y in [-1, 0, 1] ]
+    box.remove((0, 0))
+    coord_set = set()
+    for y, x in coord_list:
+        coord_set.add((y, x))
+    seed_idx = np.random.choice(np.arange(len(coord_list)), n_bootstraps)
+    val_dict = {m:list() for m in img_dict.keys()}
+    for fi, i in enumerate(seed_idx):
+        s_y, s_x = coord_list[i]
+        seed_coord = (s_y, s_x)
+        s_coord = set()
+        s_coord.add(seed_coord)
+        next_seed = set()
+        next_seed.add(seed_coord)
+        while len(s_coord) <= grow_seed: # grow random pixels until reaching 15
+            grow_set = set()
+            for ny, nx in next_seed: # grow a 3*3 chunk about current pixel
+                for dy, dx in box:
+                    grow_set.add(( ny + dy, nx + dx ))
+                    
+            if len(grow_set) == 0:
+                
+                break
+            grow_set &= coord_set
+            next_seed = grow_set - s_coord
+            s_coord |= grow_set
+            
+        mask_c = np.zeros_like(bm)
+        for x,y in s_coord:
+            mask_c[x,y] = 1
+        mask_c = mask_c > 0
+        mask_nc = np.logical_and(bm, np.logical_not(mask_c))
+        for m in img_dict.keys():
+            val_dict[m].append(calculate_CISA(img_dict[m], mask_c, mask_nc, bgl_dict[m]))
+            
+    for m in list(img_dict.keys()):
+        try:
+            df.at[m, 'miu'] = np.mean(val_dict[m])
+            df.at[m, 'sigma'] = np.std(val_dict[m])
+        except:
+            pass
+    return df
+    
+
+def calculate_CISA(img, mask_c, mask_nc, bgl):
+    area_c = np.sum(mask_c)
+    area_nc = np.sum(mask_nc)
+    sum_c = np.sum(img[mask_c])
+    sum_nc = np.sum(img[mask_nc])
+    
+    if area_c == 0 or area_nc == 0:
+        return np.nan
+    
+    if sum_nc == 0:
+        val_nc = bgl
+    else:
+        val_nc = sum_nc/area_nc
+        
+    if sum_c == 0:
+        val_c = np.minimum(bgl, val_nc)
+    else:
+        val_c = sum_c/area_c
+        
+    return np.log2(np.array(val_c/val_nc, dtype='float32'))
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # CISA
- Computational Immune Synapse Analysis
+ Computational Immune Synapse Analysis.
+
+## Example
+
+See example notebook using core A5 from the published melanoma IMC dataset by Hoch et al., 2022
+
+Hoch, Tobias, et al. "Multiplexed imaging mass cytometry of the chemokine milieus in melanoma characterizes features of the response to immunotherapy." Science immunology 7.70 (2022): eabk1692.
+
+## Dependency
+
+The core functions uses only ```numpy```, ```pandas```, ```scipy``` and ```skimage```.
+
+## Usage
+    Input (required):
+
+        img_dict: {marker: 2d image}. Input single channel images as a dictionary.
+
+        cell_df: dataframe of cell information where each row is a cell.
+
+        cellmask: 2D int ndarray. Image mask of cells. Pixel values should be integers that matches cell ID in cell_df.
+
+        boundmask: 2D int ndarray. Image mask of cell boundaries. Pixel values should be integers that matches cell ID in cell_df.
+
+        neighbor_classes: list of neighboring classes. Other classes are not included in the result dataframe, but overlaping 
+
+        expressing_markers: list of markers that are expressed in the center cell types. Their background level will be calculated as the average level in other cell types. All other markers that are not listed but included in img_dict will be treated as expressed in the center cell types and their background level will be calculated as the average level in center cell types.
+
+    Input (optional):
+        cell list: list of cell IDs of center cells to compute CISA. If None, all cells are considered.
+
+        target_classes: list of center cell types. Default: ['T'].
+
+        n_bootstrap: number of bootstraps for the null model. Default: 1000
+
+        e_width: width of dilation to calculate overlapping. Default: 2 pixels.
+
+        bound_width: width of cell boundary dilation width before calculating overlapping. 0 = use raw bound. Default: 0 pixel.
+
+        dropna: Bool, if drops invalid synapses. Default: True
+
+        class_col: the colname for the cell type column in cell_df. Default: 'Class'
+
+        cell_id_col: the colname for the cell ID column in cell_df. Default: 'Cell'
+
+        keep_cols: list of colnames that should be kept in the output dataframe. Default: []
+        
+        excluded: list of cell between which and the center cells the contacted regions should be completely ignored in calculation. One example is to reduce the influence of signals from neighboring same type of cells. Default: ['T'].
+
+    
+## Output
+Output is a dataframe where each row corresponds to a center-neighbor cell pair.
+
+For each marker ```m``` in the input, there will be several columns:
+
+```{m}_CISA``` is the calculated synapse strength between the two cells.
+
+```{m}_c``` and ```{m}_nc```: Sum of pixel intensity in the contacting (```c```) and non-contacting (```nc```) boundary areas.
+
+```{m}_miu``` and ```{m}_sigma```: Mean and std of the synapses under the null model where a random connected pixls along the cell boundary are selected as the contacting area.
+
+```{m}_z``` and ```{m}_p```: The z-score and p-value for the observed synapse strength under the null model.
+
+## Note
+
+Image preprocessing, such as de-noising, signal transformation and normalization, is determined by the user. 
+
+A large bootstrap number may significantly increase the running time. In most cases, the CISA score (as in ```{m}_CISA``` column) is sufficient to characterize the behaviour.
